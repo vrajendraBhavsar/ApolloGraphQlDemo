142496f712541ee2f58f942ec5a0f059d0369aeb
Vrajendra <Vrajendraa>
2021-10-28 19:13:40 +0530

    - added nav graph, adapter. - removed separate diffUtil class and added within adapter.

--- app/src/main/java/com/example/apollographqldemo/MainActivity.kt
+++ app/src/main/java/com/example/apollographqldemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.apollographqldemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.example.apollographqldemo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
