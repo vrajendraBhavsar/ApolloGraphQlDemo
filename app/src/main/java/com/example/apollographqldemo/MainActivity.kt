570823b2233d900caa642096048586cea8269e03
Vrajendra <Vrajendraa>
2021-11-10 11:27:16 +0530

    - Ui changes - updated graphQL version - added new query file -> parameterQuery.graphql - added CharactersListFragment.kt - updated schema file - added config file that will help testing queries within studio -> .graphqlconfig

--- app/src/main/java/com/example/apollographqldemo/MainActivity.kt
+++ app/src/main/java/com/example/apollographqldemo/MainActivity.kt
@@ -17,59 +17,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-//    private lateinit var navController: NavController
-
-    val viewModel by viewModels<CharViewModel>()    //vm
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
-//        navController = navHostFragment.findNavController()
-//        setupActionBarWithNavController(navController)  //for back button
-        //Method reference
-        viewModel.charactersList.observe(this, ::handleCharList)
-
-        binding.btnGetData.setOnClickListener {
-            viewModel.getCharactersData()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)  //for back button
     }
 
-    private fun handleCharList(viewState: ViewState<Response<CharactersDataQuery.Data>>?) {
-        when (viewState) {
-            is ViewState.Loading -> {
-//                binding.charactersRv.visibility = View.GONE
-//                binding.charactersFetchProgress.visibility = View.VISIBLE
-            }
-            is ViewState.Success -> {
-                if (viewState.value?.data?.characters?.results?.size == 0) {
-//                    characterAdapter.submitList(emptyList())
-//                    binding.charactersFetchProgress.visibility = View.GONE
-//                    binding.charactersRv.visibility = View.GONE
-//                    binding.charactersEmptyText.visibility = View.VISIBLE
-                } else {
-//                    binding.charactersRv.visibility = View.VISIBLE
-//                    binding.charactersEmptyText.visibility = View.GONE
-                }
-                val results: List<CharactersDataQuery.Result?>? =
-                    viewState.value?.data?.characters?.results
-                Log.d("APOLLOTEST", "handleCharList: ${results.toString()}")
-//                characterAdapter.submitList(results)
-//                binding.charactersFetchProgress.visibility = View.GONE
-            }
-            is ViewState.Error -> {
-//                characterAdapter.submitList(emptyList())
-//                binding.charactersFetchProgress.visibility = View.GONE
-//                binding.charactersRv.visibility = View.GONE
-//                binding.charactersEmptyText.visibility = View.VISIBLE
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
     }
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        navController.navigateUp()
-//        return super.onSupportNavigateUp()
-//    }
 }
